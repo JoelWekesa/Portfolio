1b24bd6d7517c5f0d0a099bdc2f96478ff1dffb2
JoelWekesa <67539766+JoelWekesa@users.noreply.github.com>
2021-05-11 14:16:55 +0300

    Added django-heroku configuration

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -138,3 +139,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+django_heroku.settings(locals())
