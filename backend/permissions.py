b54fe36d1f61c6bbf7629a615de9698249f3fe68
JoelWekesa <67539766+JoelWekesa@users.noreply.github.com>
2021-05-15 12:58:11 +0300

    Added permissions

--- backend/permissions.py
+++ backend/permissions.py
@@ -2,31 +2,20 @@ from rest_framework import permissions
 from rest_framework.exceptions import AuthenticationFailed
 from users.models import *
 
-# class BlockGetRequests(permissions.BasePermission):
-#     def has_permission(self, request, view):
-#         x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-#         if x_forwarded_for:
-#             ip_addr = x_forwarded_for.split(',')[0]
-#             if ip_addr == "127.0.0.1":
-#                 return True
-#         else:
-#             ip_addr = request.META.get('REMOTE_ADDR')
-#             if ip_addr == "127.0.0.1":
-#                 return True
-#         if request.method == 'POST': # Returns True if POST request
-#             return True
-#         else:
-#             raise AuthenticationFailed(f'{ip_addr}', 403)
-
-
 class BlockGetRequests(permissions.BasePermission):
+     def has_permission(self, request, view):
+        if request.method == 'POST': # Returns True if POST request
+            return True
+        else:
+            raise AuthenticationFailed(f'{ip_addr}', 403)
+
+
+class BlockGetRequestsIP(permissions.BasePermission):
     def has_permission(self, request, view):
         x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
         if x_forwarded_for:
             ip = x_forwarded_for.split(',')[0]
-            if ip == "105.162.21.42":
+            if ip == AllowedIP.objects.first():
                 return True
-        elif request.method == 'POST': # Returns True if POST request
-            return True
         else:
             raise AuthenticationFailed(f'{x_forwarded_for}', 403)
